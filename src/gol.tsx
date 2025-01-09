8592d7faaee7a43c8207f53167f686fb95959f38
jyecs <gmding777@gmail.com>
2025-01-09 10:48:57 -0800

    Console/text version of GoL works (for now)

--- src/gol.tsx
+++ src/gol.tsx
@@ -7,71 +7,73 @@ function Gol() {
             this.x = x;
             this.y = y;
         }
+
+        toStringKey() {
+            return `${this.x} ${this.y}`;
+        }
     }
 
     // Current generation of living cells
     // Uses a hashmap since I want this to eventually become an infinite grid
-    const livingCells = new Set<Point>();
-    let called = 0;
+    const livingCells = new Set<String>();
     const test1 = new Point(0,-1);
     const test2 = new Point(0,0);
     const test3 = new Point(0,1);
 
-    livingCells.add(test1);
-    livingCells.add(test2);
-    livingCells.add(test3);
+    livingCells.add(test1.toStringKey());
+    livingCells.add(test2.toStringKey());
+    livingCells.add(test3.toStringKey());
 
-    const testMap = new Map<Array<number>, number>();
-    const p1 = [0, 0]
-    const p2 = [0, 0]
-    testMap.set(p1,1);
-    console.log(testMap.get(p1));
-
-
-    
 
     // TODO: advances the generation of the next step in GoL
     const next = () => { 
-        const deltaGeneration = new Map<Point, number>();
-        livingCells.forEach((key: Point) => {
+        const deltaGeneration = new Map<String, number>();
+        livingCells.forEach((key: String) => {
             let neighbors = getNeighbors(key);
             computeNeighbors(deltaGeneration, neighbors);
         });
 
-        deltaGeneration.forEach((numNeighbors: number, cell: Point) => {
+        deltaGeneration.forEach((numNeighbors: number, cell: String) => {
             if (numNeighbors < 2 || numNeighbors > 3) {
                 livingCells.delete(cell);
-            } else {
+            } else if (numNeighbors === 3) {
                 livingCells.add(cell);
             }
         })
     };
 
-    const computeNeighbors = (gen: Map<Point, number>, neighbors: Array<Point>) => {
-        neighbors.forEach((point: Point) => {
-            if (gen.has(point)) {
-                gen.set(point, gen.get(point) + 1);
+    const computeNeighbors = (gen: Map<String, number>, neighbors: Array<Array<number>>) => {
+        neighbors.forEach((point: Array<Number>) => {
+            const cell = `${point[0]} ${point[1]}`
+            if (gen.has(cell)) {
+                gen.set(cell, gen.get(cell) + 1);
             } else {
-                gen.set(point, 1);
+                gen.set(cell, 1);
             }
         })
     }
 
-    const getNeighbors = (cell: Point) => {
+    const getNeighbors = (cell: String) => {
         const neighbors = [];
+        const coords = cell.split(" ");
         for (let i = -1; i <= 1; i++) {
             for (let j = -1; j <= 1; j++) {
                 if (!(i === 0 && j === 0)) {
-                    neighbors.push(new Point(cell.x + i, cell.y + j));
+                    neighbors.push([parseInt(coords[0]) + i, parseInt(coords[1]) + j]);
                 }
             }
         }
         return neighbors;
     }
 
+    const preLoadPoints = (points: Array<Point>) => {
+        points.forEach((point: Point) => {
+            livingCells.add(point.toStringKey());
+        })
+    }
     const getCells = () => { return livingCells };
 
-    return {next, getCells};
+    return {next, getCells, preLoadPoints};
 
 };
 export default Gol
