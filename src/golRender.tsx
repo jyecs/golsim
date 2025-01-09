461b074e3cb5ebb112e82841933d5c909d24af75
jyecs <gmding777@gmail.com>
2025-01-09 11:33:50 -0800

    Canvas properly renders

--- src/golRender.tsx
+++ src/golRender.tsx
@@ -1,8 +1,20 @@
 import React from "react";
+import { useRef, useEffect } from "react";
 
 const Renderer = () => {
+
+    const canvasRef = useRef(null);
+
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        const ctx = canvas.getContext("2d");
+
+        ctx.fillStyle = "#000000";
+        ctx.fillRect(10,10,150,150);
+    },[])
+
     return(
-        <canvas></canvas>
+        <canvas ref={canvasRef}></canvas>
     );
 }
 
