8609c47ec3e9fb9534c895baf4d48003b6512f92
jyecs <gmding777@gmail.com>
2025-01-10 20:21:15 -0800

    removed default styles, created simple grid using canvas

--- src/golRender.tsx
+++ src/golRender.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./App.css"
 import { useRef, useEffect } from "react";
 
 const Renderer = () => {
@@ -9,12 +10,33 @@ const Renderer = () => {
         const canvas = canvasRef.current;
         const ctx = canvas.getContext("2d");
 
-        ctx.fillStyle = "#000000";
-        ctx.fillRect(10,10,150,150);
+        drawGrid(ctx);
+
+
+
     },[])
+    // 1000,800 grid
+    // what type is a canvas context???
+    const drawGrid = (ctx) => {
+        const width = 1000;
+        const height  = 800;
+        const padding = 5;
+        for (let x = 0; x <= width; x += 10) {
+            ctx.moveTo(x, 0);
+            ctx.lineTo(x, height);
+        }
+
+        for (let y = 0; y < height; y+= 10) {
+            ctx.moveTo(0, y);
+            ctx.lineTo(width, y)
+        }
+        ctx.lineWidth = 2;
+        ctx.strokeStyle = "black";
+        ctx.stroke();
+    }
 
     return(
-        <canvas ref={canvasRef}></canvas>
+        <canvas height="800" width="1000" ref={canvasRef}></canvas>
     );
 }
 
