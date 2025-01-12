bd4c9a17946d4230f4d78ab1d1443f84cb64dbcc
jyecs <gmding777@gmail.com>
2025-01-11 17:40:38 -0800

    Made it so the canvas has an acceptable look

--- src/golRender.tsx
+++ src/golRender.tsx
@@ -10,44 +10,52 @@ const Renderer = () => {
         const canvas = canvasRef.current;
         const ctx = canvas.getContext("2d");
 
+        const dpr = window.devicePixelRatio || 1;
+        const width = canvas.width;
+        const height = canvas.height;
+        canvas.width = width * dpr;
+        canvas.height = height * dpr;
+        canvas.style.width = `${width}px`;
+        canvas.style.height = `${height}px`;
+
         drawGrid(ctx);
-        drawCell(ctx, 500, 250);
+        drawCell(ctx, 1000, 800);
 
 
 
     },[])
     // 1000,800 grid
     const drawGrid = (ctx: CanvasRenderingContext2D) => {
-        const width = 1000;
-        const height  = 800;
-        const padding = 5;
+        const width = 1040;
+        const height  = 840;
+        const padding = 10;
+
         ctx.lineWidth = 1;
         ctx.strokeStyle = "grey";
-        for (let x = 0; x <= width; x += 10) {
+        for (let x = 0; x < width; x += 10 + padding) {
             ctx.beginPath();
-            ctx.moveTo(x, 0);
-            ctx.lineTo(x, height);
+            ctx.moveTo(x + padding, padding);
+            ctx.lineTo(x + padding, height - padding);
             ctx.closePath();
             ctx.stroke();
         }
 
-        for (let y = 0; y <= height; y+= 10) {
+        for (let y = 0; y < height; y+= 10 + padding) {
             ctx.beginPath();
-            ctx.moveTo(0, y);
-            ctx.lineTo(width, y)
+            ctx.moveTo(padding, y + padding);
+            ctx.lineTo(width - padding, y + padding)
             ctx.closePath();
             ctx.stroke()
         }
     }
 
     const drawCell = (ctx: CanvasRenderingContext2D, x: number, y: number) => {
-        ctx.strokeStyle = "black";
         ctx.fillStyle = "green";
-        ctx.fillRect(x+0.6,y+0.6,8.3,8.3);
+        ctx.fillRect(x - 10,y - 10,20,20);
     }
 
     return(
-        <canvas height="800" width="1000" ref={canvasRef}></canvas>
+        <canvas height="840" width="1040" ref={canvasRef}></canvas>
     );
 }
 
