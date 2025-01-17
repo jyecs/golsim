e5b5c5729aaf7a8e87876bff454d9daee537aeab
jyecs <gmding777@gmail.com>
2025-01-17 15:35:54 -0800

    Added grid dragability, but broke cell drawing

--- src/App.tsx
+++ src/App.tsx
@@ -16,9 +16,14 @@ function App() {
   const [presetGetter, setPresetGetter] = useState(preset());
   const [presetName, setPresetName] = useState<string>("Presets")
   const [playButton, setPlayButton] = useState<string>("Start");
+  const [numGenerations, setNumGenerations] = useState<number>(0);
+  const [offset, setOffset] = useState({x: 0, y: 0});
+  const [dragging, setDragging] = useState(false);
+  const [lastMousePosition, setLastMousePosition] = useState<{x: number; y: number} | null>(null);
 
   const nextGeneration = () => {
     const change = gameOfLife.next();
+    setNumGenerations((prev) => prev + 1);
     change.forEach((type: Boolean, cell: string) => {
       drawCell(ctx, cell, type);
     })
@@ -35,11 +40,11 @@ function App() {
     canvas.height = height;
     canvas.style.width = `${width}px`;
     canvas.style.height = `${height}px`;
+    gameOfLife.preLoadPoints(presetGetter.getCurrentPreset());
     const eventWrapper = (event) => {
       handleClick(event, ctx);
     }
     canvas.addEventListener("click", eventWrapper);
-    drawBoard(presetGetter.getCurrentPreset(),ctx, width, height);
 
     return () => {
       canvas.removeEventListener("click", eventWrapper);
@@ -58,6 +63,12 @@ function App() {
     }
 
   },[isRunning])
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+    drawBoard(ctx, offset.x, offset.y);
+  }, [offset])
 
   const handleClick = (event, ctx: CanvasRenderingContext2D) => {
     const canvas = canvasRef.current;
@@ -87,13 +98,36 @@ function App() {
     resetBoard();
   }
 
+  const handleMouseDown = (e: React.MouseEvent) => {
+    setDragging(true);
+    setLastMousePosition({x: e.clientX, y: e.clientY});
+  }
+
+  const handleMouseUp = () => {
+    setDragging(false);
+    setLastMousePosition(null);
+  }
+
+  const handleMouseMove = (e: React.MouseEvent) => {
+    if (dragging && lastMousePosition) {
+      const deltaX = e.clientX - lastMousePosition.x;
+      const deltaY = e.clientY - lastMousePosition.y;
+
+      setOffset((prev) => ({
+        x: prev.x + deltaX,
+        y: prev.y + deltaY
+      }));
+
+      setLastMousePosition({x: e.clientX, y: e.clientY})
+    }
+  }
+
   const toggleDrawing = ()=> {
     (playButton === "Start") ? setPlayButton("Pause") : setPlayButton("Start")
     setIsRunning(!isRunning);
   }
 
-  const drawBoard = (points: Array<Point>, ctx: CanvasRenderingContext2D, width: number, height: number) => {
-    gameOfLife.preLoadPoints(points);
+  const drawBoard = (ctx: CanvasRenderingContext2D, width: number, height: number) => {
     const cells = gameOfLife.getCells();
 
     drawGrid(ctx, width, height);
@@ -104,22 +138,29 @@ function App() {
 
 
   // 1000,800 grid
-  const drawGrid = (ctx: CanvasRenderingContext2D, width: number, height: number) => {
+  const drawGrid = (ctx: CanvasRenderingContext2D, offsetX: number, offsetY: number) => {
+    const height = ctx.canvas.height;
+    const width = ctx.canvas.width;
+    const gridSize = 20;
     const padding = 10;
+
+    ctx.clearRect(0,0, width, height);
+    const startX = (offsetX % gridSize) + padding;
+    const startY = (offsetY % gridSize) + padding;
 
     ctx.lineWidth = 1;
     ctx.strokeStyle = "grey";
-    for (let x = 0; x < width; x += 10 + padding) {
+    for (let x = startX; x < width - padding; x += gridSize) {
       ctx.beginPath();
-      ctx.moveTo(x + padding, padding);
-      ctx.lineTo(x + padding, height - padding);
+      ctx.moveTo(x, padding);
+      ctx.lineTo(x, height - padding);
       ctx.closePath();
       ctx.stroke();
     }
-    for (let y = 0; y < height; y+= 10 + padding) {
+    for (let y = startY; y < height - padding; y+= gridSize) {
       ctx.beginPath();
-      ctx.moveTo(padding, y + padding);
-      ctx.lineTo(width - padding, y + padding)
+      ctx.moveTo(padding, y);
+      ctx.lineTo(width - padding, y)
       ctx.closePath();
       ctx.stroke()
     }
@@ -129,14 +170,16 @@ function App() {
     const canvas = canvasRef.current;
     const width = canvas.width;
     const height = canvas.height;
+    setNumGenerations(0);
     setIsRunning(false);
     gameOfLife.clear();
     resetCells();
     setPlayButton("Start");
-    drawBoard(presetGetter.getCurrentPreset(),ctx, width, height);
+    drawBoard(ctx, width, height);
   }
 
   const clearBoard = () => {
+    setNumGenerations(0);
     gameOfLife.clear();
     setIsRunning(false);
     setPlayButton("Start");
@@ -154,8 +197,8 @@ function App() {
   // Make sure that this is change later such that the cells draw okay.
   const drawCell = (ctx: CanvasRenderingContext2D, cell: String, type: Boolean) => {
     const coords = cell.split(" ");
-    const x = Number.parseInt(coords[0]);
-    const y = Number.parseInt(coords[1]);
+    const x = Number.parseInt(coords[0]) + offset.x;
+    const y = Number.parseInt(coords[1]) + offset.y;
     if (x > 50 || x < 0) { return; }
     if (y < 0 || y > 40) { return; }
 
@@ -171,7 +214,8 @@ function App() {
 
   return (
     <div className="MainContainer">
-      <canvas height="840" width="1040" ref={canvasRef}></canvas>
+      <canvas height="840" width="1040" ref={canvasRef} onMouseDown={handleMouseDown} onMouseMove={handleMouseMove}
+       onMouseUp={handleMouseUp} onMouseLeave={handleMouseUp}></canvas>
       <div className="RightContainer">
         <h1>
           <div>Conway's</div>
@@ -188,6 +232,7 @@ function App() {
             <PresetDropdown presets={presetGetter.listPrests()} onSelect={handleDropdownSelect} currPreset={presetName}></PresetDropdown>
             <Button size="lg" onClick = {resetBoard}>Reset</Button>
           </div>
+          <div>Generations: {numGenerations}</div>
         </div>
       </div>
     </div>
